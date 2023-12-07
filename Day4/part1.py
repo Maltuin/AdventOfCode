e20feb7b1c7bd1a62f183505c0aed72126f8d4b6
Maxime Margaine <maxime.margaine@gmail.com>
2023-12-07 16:22:59 +0100

    correct Day4

--- Day4/part1.py
+++ Day4/part1.py
@@ -3,14 +3,13 @@ import numpy as np
 
 total = 0
 
-for x in open("data.txt", "r"):
-    first_split = x.split(":")
-    numbers = first_split[1].split("|")
-    a = np.intersect1d(np.array(re.findall("\d+", numbers[0])), np.array(re.findall("\d+", numbers[1])))
+regex = "\d+(?![\d+:])"
+
+for line in open("data.txt", "r"):
+    numbers = line.split("|")
+    a = np.intersect1d(np.array(re.findall(regex, numbers[0])), np.array(re.findall(regex, numbers[1])))
     length = len(a)
-    if length == 1:
-        total += 1
-    if length > 1:
+    if(length > 0):
         total += 2**(length - 1)
 
 print(total)
--- Day4/part2.py
+++ Day4/part2.py
@@ -2,12 +2,13 @@ import re
 import numpy as np 
 
 results = np.ones((188,), dtype=int)
+regex = "\d+(?![\d+:])"
+
 count = 0
 
-for x in open("data.txt", "r"):
-    first_split = x.split(":")
-    numbers = first_split[1].split("|")
-    length = len(np.intersect1d(np.array(re.findall("\d+", numbers[0])), np.array(re.findall("\d+", numbers[1]))))
+for line in open("data.txt", "r"):
+    numbers = line.split("|")
+    length = len(np.intersect1d(np.array(re.findall(regex, numbers[0])), np.array(re.findall(regex, numbers[1]))))
     for x in range(length):
         results[x + count + 1] += results[count]
     count += 1
