669f173a46af7b54ade348961764e44fec16bec0
Maxime M <maxime.margaine@groupeseloger.com>
2023-12-19 18:05:11 +0100

    add day 13 part 2

--- Day13/part2.py
+++ Day13/part2.py
@@ -1,15 +1,16 @@
-def get_sym(blocklines):
-    for x in range(1, len(blocklines) -1):
+def get_sym(blocklines, line):
+    for x in range(1, len(blocklines)):
         firsthalf = blocklines[:x][::-1]
         secondhalf = blocklines[x:]
+        size = min(len(firsthalf), len(secondhalf))
 
         same = True
-        for y in range(0, min(len(firsthalf), len(secondhalf))):
+        for y in range(0, size):
             if(firsthalf[y] != secondhalf[y]):
                 same = False
                 break
 
-        if(same):
+        if(same and line > x - size and line < x + size):
             return x
     
     return 0
@@ -21,18 +22,20 @@ def boucle(blocklines):
             line = list(temp[x])
             line[y] = "." if line[y] == "#" else "#"
             temp[x] = "".join(line)
-            hor = get_sym(temp)
-            if hor != 0:
-                return hor * 100
 
-            ver = get_sym(list(zip(*temp)))
-            if ver != 0:
-                return ver
+            res = get_sym(temp, x)
+
+            if res != 0:
+                return res
     return 0
 
 result = 0
 
 for block in open("data.txt", "r").read().split("\n\n"):
-    result += boucle(block.splitlines())
+    lines = block.splitlines()
+    
+    hor = boucle(lines)
+    
+    result += hor * 100 if hor != 0 else boucle(list(map(lambda x: "".join(x), zip(*lines))))
 
 print(result)
